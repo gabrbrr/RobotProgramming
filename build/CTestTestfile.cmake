5a2851b82b792806be2d25db90d0d6e7edcf90ec
ggabrbrr <pamfil.1938844@studenti.uniroma1.it>
2025-03-22 15:01:22 +0100

    build canc

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/gabrbrr/Desktop/robot_programming/RobotProgramming/src
-# Build directory: /home/gabrbrr/Desktop/robot_programming/RobotProgramming/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("dmap_localizer")
--- build/gtest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest
-# Build directory: /home/gabrbrr/Desktop/robot_programming/RobotProgramming/build/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
